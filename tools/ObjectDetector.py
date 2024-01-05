8ac8266c9232f3a6a9a4544af1026cd948fcb869
X_Z <52037372+tong-xz@users.noreply.github.com>
2024-01-04 23:43:34 -0500

    train again with another env

--- tools/ObjectDetector.py
+++ tools/ObjectDetector.py
@@ -65,7 +65,7 @@ class Detector(object):
 if __name__ == '__main__':
 
     image_path = '../13A.jpg'
-    detector = Detector('../results/detect/train25/weights/best.pt')
+    detector = Detector('../runs/detect/train3/weights/best.pt')
     result = detector.inference(image_path)
     box_dict = detector.get_all_boxes(result)
 
--- train.sh
+++ train.sh
@@ -1,2 +1,1 @@
-pip install ultralytics
 yolo task=detect mode=train model=yolov8m.pt data=./GrapeYOLO-2/data.yaml epochs=30 imgsz=640
